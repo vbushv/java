cd876e9c276f54b0dc105a30c3afef2d8615f122
bkige <bkige@192.168.219.109>
2022-12-09 00:25:22 +0900

    test2,3

---
+++ designpattern/src/test2.java
@@ -0,0 +1,22 @@
+
+public class test2 {
+
+	public static void main() {
+		
+		System.out.println("aaaaaa");
+
+		System.out.println("dev222222");
+		System.out.println("dev222222");
+
+		System.out.println("dev");
+		System.out.println("dev1111");
+		System.out.println("dev2222"
+				+ "");
+
+		//test2
+		//test2
+		
+		//sdfg234235
+	}
+	
+}
---
+++ designpattern/src/test3.java
@@ -0,0 +1,22 @@
+
+public class test3 {
+
+	public static void main() {
+		
+		System.out.println("aaaaaa");
+
+		System.out.println("dev222222");
+		System.out.println("dev222222");
+
+		System.out.println("dev");
+		System.out.println("dev1111");
+		System.out.println("dev2222"
+				+ "");
+
+		//test3
+		//test3333333333
+		
+		//sdfg234235
+	}
+	
+}
