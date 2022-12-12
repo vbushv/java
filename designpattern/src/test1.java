219b2b53e33ef8cb9323f54ef0c841cd223227a5
bkige <bkige@192.168.219.111>
2022-12-12 12:08:45 +0900

    4321

--- designpattern/src/test1.java
+++ designpattern/src/test1.java
@@ -8,6 +8,17 @@ public class test1 {
 		System.out.println("dev222222");
 		System.out.println("dev222222");
 
+		
+		//515
+		//235235432
+		
+		System.out.println("dev2afaf14222");
+		System.out.println("dev212241242222");
+		System.out.println("dev22222142");
+		System.out.println("dev2222aw3aw3422");
+
+		
+		
 		System.out.println("dev");
 		System.out.println("dev1111");
 		System.out.println("dev2222"
--- designpattern/src/test2.java
+++ designpattern/src/test2.java
@@ -8,6 +8,16 @@ public class test2 {
 		System.out.println("dev222222");
 		System.out.println("dev222222");
 
+		//515
+		//235235432
+		//0000000000000000000
+		
+		System.out.println("dev2afaf14222");
+		System.out.println("dev21224177777772");
+		System.out.println("dev5555555552");
+		System.out.println("dev2222aw55555553422");
+		
+		
 		System.out.println("dev");
 		System.out.println("dev1111");
 		System.out.println("dev2222"
